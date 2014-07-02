10701b1a9c0967819f445c681997398b2153e890
yautah <yautah2008@gmail.com>
2014-07-02 13:51:30 +0800

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinyq::Application.config.secret_token = '38c2ee3fbe356aef1e1ce87b5119a449f59b37b84f98961f1efdfc197118ccde41ee31ff8846d47318de55a5616b1bb81615e2e6b4fbe896ec5ae8d45d28c6f5'
